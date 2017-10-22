cd2ae37f8636809cd0a3e8304a4436343eb9a7d5
Christopher Polito <cpolito343@gmail.com>
2017-10-22 16:56:28 -0400

    university comedians

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UIRouterModule, UIView } from '@uirouter/angular';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { APP_ROUTES } from './app.states';
+// import { routerConfigFn } from './router.config';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UIRouterModule.forRoot({
+      states: APP_ROUTES,
+      useHash: true,
+      otherwise:{state:"home"}
+  	}),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.states.ts
@@ -0,0 +1,12 @@
+
+
+import { HomeComponent } from "./components/home/home.component";
+import { AppComponent } from "./app.component";
+
+
+export const APP_ROUTES = [
+{
+  name: 'home',
+  component: HomeComponent,
+}
+]
